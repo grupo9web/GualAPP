056a58af68a5ebc1f0951b2c31495f3c5abc1df6
jpguirado <43378763+jpguirado@users.noreply.github.com>
2018-12-03 11:21:38 +0100

    Update WallApp.js

--- WallApp.js
+++ WallApp.js
@@ -728,13 +728,13 @@ function getMobileOperatingSystem() {
         android = true;
         window.alert("2: " + android);
 
-        /*
+        
         document.getElementsByClassName("canvas").style.height = "auto";
-        document.getElementsByClassName("canvas").style.width = "150%";
-        */
+        document.getElementsByClassName("canvas").style.width = "100%";
+        /*
         document.getElementById("lienzo").style.height = "auto";
         document.getElementById("lienzo").style.width = "100%";
-
+        */
         //document.documentElement.requestFullscreen();
         //window.scrollTo(0,1);
 
